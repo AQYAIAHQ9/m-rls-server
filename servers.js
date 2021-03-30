087709b36d40c9b56863d1dd881f68bc3f1bb9b1
AQYAIAHQ9 <70981697+AQYAIAHQ9@users.noreply.github.com>
2021-03-30 12:09:24 -0300

    Update servers.js

--- servers.js
+++ servers.js
@@ -2,3 +2,10 @@ const server1 = require ('./server1')
 //aplicação do servidor 1
 const seever2 = require ('./server2')
 //aplicação do servidor 2
+
+const slotweb = require('./index.html')
+//configurações do servidor 
+
+const server = new ('m901.slotweb.ga')
+const web = require = ('./web.json')
+//configurações da máquina

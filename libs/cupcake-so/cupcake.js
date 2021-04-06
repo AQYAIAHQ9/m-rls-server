acf41c0ca5342337a2647b882f5a25e308e13eed
AQYAIAHQ9 <70981697+AQYAIAHQ9@users.noreply.github.com>
2021-04-06 00:13:12 -0300

    Create cupcake.js

---
+++ libs/cupcake-so/cupcake.js
@@ -0,0 +1,13 @@
+//######################################
+
+//Cupcake V4.0s @ 32bits (SO)
+//by: TROLLGAMESPLAY
+
+
+    if(Cupcake, Microcraft)
+    new database = cupcake
+     //DAta Base
+     
+const TROLLGAMES = require ('cupcake.js'() =>{
+    trollgames.cupcake('BR');
+})
